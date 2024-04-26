62eb88085f5ccc7c5031eb72e11b8890c4fd3dba
Ben Hill <benjaminjhill@icloud.com>
2024-04-25 21:02:35 -0500

    added form and blog js functionality

---
+++ assets/js/blog.js
@@ -0,0 +1,29 @@
+const blogSection = document.querySelector('.blog-section');
+
+let blogs = localStorage.getItem('blogPosts');
+let allBlogs = JSON.parse(blogs)
+
+for (let blog of allBlogs) {
+    let newBlog = document.createElement("div");
+    let blogTitle = document.createElement("h2")
+    let blogContent = document.createElement("p")
+    let authorTag = document.createElement("p")
+
+    blogTitle.textContent = blog.title;
+    blogContent.textContent = blog.content;
+    authorTag.textContent = "Posted by: " + blog.username;
+
+    newBlog.setAttribute('class', 'blog-card');
+    blogTitle.setAttribute('class', 'blog-title');
+    blogContent.setAttribute('class', 'blog-content');
+    authorTag.setAttribute('class', 'author-tag');
+
+
+    blogSection.appendChild(newBlog);
+    newBlog.appendChild(blogTitle);
+    newBlog.appendChild(blogContent);
+    newBlog.appendChild(authorTag);
+
+
+    console.log(blog);
+}
---
+++ assets/js/form.js
@@ -0,0 +1,39 @@
+const username = document.querySelector('#username');
+const title = document.querySelector('#title');
+const content = document.querySelector('#content');
+const submitButton = document.querySelector('#submit');
+
+
+submitButton.addEventListener('click', function (event) {
+    let previousPosts = localStorage.getItem('blogPosts')
+    let blogPosts = [];
+
+    if (username.value === ''){
+        window.alert("You must fill out username to submit!");
+        event.preventDefault();
+        return;
+    } else if (title.value === ''){
+        window.alert("You must fill out username to submit!");
+        event.preventDefault();
+        return;
+    } else if (content.value === ''){
+        window.alert("You must fill out username to submit!");
+        event.preventDefault();
+        return;
+    }
+    
+    if (previousPosts !== null && previousPosts !== undefined) {
+        blogPosts = JSON.parse(previousPosts);
+    }
+    const blogInfo = {
+        username: username.value.trim(),
+        title: title.value.trim(),
+        content: content.value.trim(),
+    };
+
+    blogPosts.push(blogInfo);
+
+    localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
+    event.preventDefault();
+    window.location.href="file:///Users/benjaminhill/bootcamp/challenges/personal-blog-challenge/blog.html"
+});
